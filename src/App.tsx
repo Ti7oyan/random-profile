7defa227fb4c620c2bbc70eb1c29fcde186a803b
Ti7oyan <ticiano.morvan@protonmail.com>
2022-06-03 00:57:32 -0300

    stable version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Component, createSignal, createEffect, createMemo } from 'solid-js'
+import { Component, createSignal, createMemo, splitProps } from 'solid-js'
 import styles from './App.module.scss'
 import getRandomPerson from './getRandomPerson'
 import IPerson from './types'
 
+type InformationType = {
+  name: string;
+  value: string;
+}
+
+const InformationSection = (props: InformationType) => {
+  const [local] = splitProps(props, ['name', 'value'])
+  return (
+    <section class={styles.informationSection} id={local.name}>
+      <h3>{local.name}</h3>
+      <p>{local.value}</p>
+    </section>
+  )
+}
+
 const App: Component = () => {
   const initialValue = createMemo(() => getRandomPerson())
   const [person, setPerson] = createSignal<IPerson>(initialValue())
+
+  const handleClick = () => createMemo(() => setPerson(getRandomPerson()))
 
   return (
     <>
@@ -18,7 +35,9 @@ const App: Component = () => {
             src={person().avatar}
             alt={`${person().name}'s profile picture.`}
           />
+
           <h1>{person().name}</h1>
+
           <h2>
             {person().userName}
             {' '}
@@ -27,7 +46,26 @@ const App: Component = () => {
             <a href={`mailto:${person().email}`}>
               {person().email}
             </a>
+            {' '}
+            -
+            {' '}
+            {person().gender}
           </h2>
+
+          <div class={styles.informationContainer}>
+            <InformationSection name='Phone' value={person().phone} />
+            <InformationSection name='Address' value={`${person().address.city} - ${person().address.country}`} />
+            <InformationSection name="Works in" value={`${person().work.department} at ${person().work.name}`} />
+            <InformationSection name="Birthday" value={person().birthDate.toUTCString()} />
+            <InformationSection name="Music" value={`Genre: ${person().favourites.genre} - Song: ${person().favourites.song}`} />
+            <InformationSection name="Vehicle" value={`${person().vehicle.name} - ${person().vehicle.vrm}`} />
+          </div>
+
+          <button class={styles.button} type="button" onClick={handleClick}>
+            Re-roll person
+          </button>
+
+          <p>Made by Ticiano Morvan</p>
         </section>
       </div>
     </>
